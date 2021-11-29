5c24baa2aa844e14a6b33d80571dc70cea8769d4
Maria <maruskavadimovna@gmail.com>
2021-11-29 08:06:23 +0200

    Added getting posts functionality

--- controllers/api/v1/secure/posts/index.js
+++ controllers/api/v1/secure/posts/index.js
@@ -1,9 +1,11 @@
 import express from 'express';
 
 import create from './create';
+import get from './get';
 
 const router = express.Router();
 
 router.post('/', create);
+router.get('/:postId', get);
 
 export default router;
